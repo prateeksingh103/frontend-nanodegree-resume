f4afc8a0a35251b28a97654585176dac6d867ba0
Prateek <meshenno1@gmail.com>
2015-11-12 20:32:16 +0530

    replace and append function in resumeBuilder.js

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,21 @@
+
+//var firstName = "james";
+//var age = 32;
+
+//console.log(firstName);
+
+//var awsomeThoughts ="I am prateek and I am awesome";
+//console.log(awsomeThoughts);
+//var funThoughts = awsomeThoughts.replace("awesome","FUN");
+//console.log(funThoughts);
+//$("#main").append(funThoughts);
+
+
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+
+var formattedName = HTMLheaderName.replace("%data%","Prateek Singh");
+var formattedRole = HTMLheaderRole.replace("%data%","Web Developer");
+$("#header").append(formattedName);
+$("#header").append(formattedRole);
